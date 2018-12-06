07e93279f2a5ec1c642ec56d181c5555abe9ae79
vicb1 <vbajenaru@gmail.com>
2018-12-06 10:45:01 -0800

    Update testcgi.py

--- Windows/full_installation/htdocs/testcgi.py
+++ Windows/full_installation/htdocs/testcgi.py
@@ -4,23 +4,5 @@ print("")
 print("<html><head>")
 print("")
 print("</head><body>")
-print("Hello from Python.")
+print("Hello from Python!")
 print("</body></html>")
-
-import pandas as pd
-import numpy as np
-
-print('asd')
-s = pd.Series([1,3,5,np.nan,6,8])
-dates = pd.date_range('20130101', periods=6)
-df = pd.DataFrame(np.random.randn(6,4), index=dates, columns=list('ABCD'))
-
-print(df)
-print("Content-type: text/html")
-print("<html><head>")
-print("")
-print("</head><body>")
-print("Hello.")
-print("</body></html>")
-
-print(df.to_html())
